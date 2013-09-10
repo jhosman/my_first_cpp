3805668c3819c0d8bfd6149d21ecaff4d3b01859
Jessica Hosman <jhosman@oreilly.com>
2013-09-09 23:44:43 -0400

    Adding storage.cpp file that measures size of data types allowed by compiler

---
+++ storage.cpp
@@ -0,0 +1,25 @@
+/*
+ * storage_size.cpp
+ *
+ * The output of this program is dependant on the compiler
+ * and how much storage it provides for each data type given.
+ *
+ *  Created on: Sep 9, 2013
+ *      Author: jhosman
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\nData Type  Bytes";
+	cout << "\n---------  -----";
+	cout << "\nint           " << sizeof(int);
+	cout << "\nchar          " << sizeof(char);
+	cout << "\nbool          " << sizeof(bool);
+	cout << '\n';
+
+    system("Pause");
+	return 0;
+}

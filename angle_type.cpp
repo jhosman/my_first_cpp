203d8a3a7bf5bda5df0ed28316b99db26ab482f3
Jessica Hosman <jhosman@oreilly.com>
2013-09-29 16:28:47 -0400

    Adding angle_type.cpp file

---
+++ angle_type.cpp
@@ -0,0 +1,27 @@
+/*
+  angle_type.cpp
+  
+  From Exercise 4.3-1 in textbook.
+  This program takes an angle as input and tells the user what type of angle it is.
+*/
+
+#include <iostream>
+using namespace std;
+    
+int main ()
+    {
+        double angle;
+        
+        cout << "Enter an angle: ";
+        cin >> angle; 
+        
+        if (angle < 90)
+          cout << "It's an acute angle!";
+        else if (angle > 90)
+          cout << "It's an obtuse angle!";
+        else if (angle == 90)
+          cout << "It's a right angle!";
+        
+        system("Pause");
+        return 0;
+    } 

c456223e959388e5eb71f0dd37dfd2ace47fb093
Jessica Hosman <jhosman@oreilly.com>
2013-09-29 15:32:22 -0400

    Adding even_or_odd.cpp file

---
+++ even_or_odd.cpp
@@ -0,0 +1,26 @@
+/*
+  even_or_odd.cpp
+  
+  This program solves Exercise 4.2-7 in the textbook.
+  It takes a number input by the user and tells the user
+  if the number is even or odd.
+*/
+
+#include <iostream>
+using namespace std;
+    
+int main ()
+    {
+        int num1;
+        
+        cout << "Enter a number: ";
+        cin >> num1;
+        
+        if (num1 % 2 > 0)
+          cout << "The number " << num1 << " is odd.";
+        else 
+          cout << "The number " << num1 << " is even.";
+        
+        system("Pause");
+        return 0;
+    } 

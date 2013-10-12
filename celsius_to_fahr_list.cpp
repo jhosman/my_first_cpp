c64a2524998f18df8e5f2173ebdb881d588cc549
Jessica Hosman <jhosman@oreilly.com>
2013-10-12 16:21:14 -0400

    Adding celsius to fahr list program

---
+++ celsius_to_fahr_list.cpp
@@ -0,0 +1,35 @@
+/*
+  Program 5.4 from textbook.
+  This program converts Celsius to Fahrenheit in a list, from 5 to 50 degrees Celsius.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	const int MAXCELSIUS = 50;
+	const int STARTVAL = 5;
+	const int STEPSIZE = 5;
+	int celsius;
+	double fahren;
+	
+	cout << "DEGREES    DEGREES\n";
+	cout << "CELCIUS   FAHRENHEIT\n";
+	cout << "-------   ----------\n";
+	
+	celsius = STARTVAL;
+	
+	cout << setiosflags(ios::showpoint) << setiosflags(ios::fixed) 
+	     << setprecision(2);
+	     
+    while (celsius <= MAXCELSIUS)
+    {
+    	fahren = (9.0/5.0) * celsius + 32.0;
+    	cout << setw(4) << celsius
+    	     << setw(13) << fahren << endl;
+    	celsius = celsius + STEPSIZE;
+    }
+    
+return 0;
+}

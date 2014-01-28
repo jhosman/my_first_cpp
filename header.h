6f0149b377b8721f74c2f787918c3e7c2d90f229
jhosman-test <jhoz@lunarmail.net>
2014-01-28 12:07:09 -0500

    Create header.h

---
+++ header.h
@@ -0,0 +1,12 @@
+/*!
+* \file client.h
+*/
+
+#ifndef WORLDBOX_CLIENT_H
+#define WORLDBOX_CLIENT_H
+
+#include <worldbox/worldbox.h>
+#include <worldbox/core.h>
+#include <worldbox/graphics.h>
+
+#endif /* WORLDBOX_CLIENT_H */
